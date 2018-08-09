1c1bb8281302da4bb91c69c6d8ea7b59cc1d454b
wf2311 <wf2311@163.com>
2018-08-09 15:42:30 +0800

    修改日志保存路径

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,7 +1,7 @@
 project_name=$1
 port=$2
 echo $port
-log_dir="/logs/docker/images/$1"
+log_dir="/logs/docker/container/$1"
 log_file="$log_dir/server.log"
 if [ ! -d "$log_dir" ]; then
   mkdir $log_dir
